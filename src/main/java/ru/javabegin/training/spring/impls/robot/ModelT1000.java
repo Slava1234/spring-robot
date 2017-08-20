279badeee11180e2996a9a119c26e5bd478c3329
slava <slava_eclipse@mail.ru>
2017-08-20 15:14:16 +0600

    initial commit

---
+++ src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
@@ -0,0 +1,111 @@
+package ru.javabegin.training.spring.impls.robot;
+
+import ru.javabegin.training.spring.interfaces.Hand;
+import ru.javabegin.training.spring.interfaces.Head;
+import ru.javabegin.training.spring.interfaces.Leg;
+import ru.javabegin.training.spring.interfaces.Robot;
+
+public class ModelT1000 implements Robot {
+
+    private Hand hand;
+    private Leg leg;
+    private Head head;
+    
+    private String color;
+    private int year;
+    private boolean soundEnabled;
+
+    public ModelT1000() {
+    }
+
+    public ModelT1000(Hand hand, Leg leg, Head head) {
+        super();
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+    }
+
+    public ModelT1000(String color, int year, boolean soundEnabled) {
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+    
+    public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    @Override
+    public void action() {
+        head.calc();
+        hand.catchSomething();
+        leg.go();
+    }
+
+    @Override
+    public void dance() {
+        System.out.println("T1000 is dancing!");
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
+    }
+    
+    public void initObject() {
+        System.out.println("init");
+    }
+    
+    public void destroyObject() {
+        System.out.println("destroy");
+    }
+
+}
